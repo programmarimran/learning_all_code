84a86f8d295bb39cdaaf2dc8bac7ed3f1b56cedd
programmarimran <programmarimran@gmail.com>
2025-03-26 16:26:51 +0600

    added a cricket run handle in the  react

--- Milestone 7/module 39/event-first-project/src/counter.jsx
+++ Milestone 7/module 39/event-first-project/src/counter.jsx
@@ -2,19 +2,46 @@ import { useState } from "react"
 
 export default function Counter(){
     const [state,counter]=useState(0)
+    const [stateSix ,counterSix]=useState(0)
+    const [stateFour,counterfour]=useState(0)
+    function handleSix(){
+        const countSix =stateSix+1;
+        counterSix(countSix)
+        const six =state+6;
+        counter(six)
+    }
+    function handleFour(){
+        const countFour =stateFour+1;
+        counterfour(countFour)
+        const four=state+4;
+        counter(four)
+    }
     function handleClick(){
         const con=state+1
         counter(con)
     }
+    function handleRemove(){
+        const remve =state-1;
+        counter(remve)
+    }
+   
     const style ={
         border:'2px solid green'
         
     }
     return(
         <div style={style}>
-            <h1>count :{state} </h1>
+            {state>50 && <h4>score 50++</h4>}
+            <h3>Total six: {stateSix} </h3> 
+            <h3>Total Four: {stateFour} </h3>
+            <h1>count : {state} </h1>
             <button onClick={handleClick}>add</button>
+            <button onClick={handleSix}>add 6</button>
+            <button onClick={handleFour}>add 4</button><br /><br />
+            <button onClick={handleRemove}>1 remove</button>
 
         </div>
     )
 }
+
+
