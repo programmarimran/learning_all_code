17da938a5a8fa4c34f1031da343439f0795abcab
programmarimran <programmarimran@gmail.com>
2025-03-29 12:54:55 +0600

    added a conditional visited and not visited is complited clicket the change the bgcolor

--- Milestone 7/module 40/rest-countries-project/src/countries/country/singleCountry/singlecountry.jsx
+++ Milestone 7/module 40/rest-countries-project/src/countries/country/singleCountry/singlecountry.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Singlecountry = ({singleCountry}) => {
-   
+   const [visited,setVisited]=useState(false)
     const handleClick=()=>{
-        console.log("click hoise")
+        // setVisited(true)
+        // if(visited===true){
+        //     setVisited(false)
+        // }
+        // else{
+        //     setVisited(true)
+        // }
+        setVisited(!visited)
     }
     return (
-        <div className="border border-4  border-red-500 rounded-2xl flex flex-col justify-between gap-3 p-6">
-            <h1 className='text-center text-base font-normal'>Name:{singleCountry.name.common} </h1>
+        <div className={`border border-4  border-red-500 rounded-2xl flex flex-col justify-between gap-3 p-6 ${visited&&"bg-yellow-50"}`}>
+            <h1 className={`text-center text-base font-normal ${visited &&"text-black"}`}>Name:{singleCountry.name.common} </h1>
       
            <img src={singleCountry.flags.png} alt="" />
       
-           <button onClick={handleClick}> not click</button>
+           <button onClick={handleClick}> 
+            {visited?"visited":"not visited"}
+          
+           </button>
       
            
         </div>
