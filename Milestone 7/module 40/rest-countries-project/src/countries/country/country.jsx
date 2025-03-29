36eb3a5f2081eff398f3ac8d9e10a8445861103c
programmarimran <programmarimran@gmail.com>
2025-03-29 16:35:51 +0600

    added a commit -

--- Milestone 7/module 40/rest-countries-project/src/countries/country/country.jsx
+++ Milestone 7/module 40/rest-countries-project/src/countries/country/country.jsx
@@ -1,14 +1,23 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Singlecountry from './singleCountry/singlecountry';
 
 const Country = ({countrys}) => {
     const country=use(countrys)
+    const [visitedCountry,setVisitedCountry]=useState([])
+    const handlevisitedCountry=()=>{
+        console.log("set hoise visitedcountry")
+    }
     
     return (
-        <div className='grid grid-cols-3 gap-4'>
+        <div >
+            <h1 className='m-8'>Visited Country: {}</h1>
+
+            <div className='grid grid-cols-3 gap-4'>
             {
                 country.map(singleCountry=><Singlecountry key={singleCountry.flags.png} singleCountry={singleCountry}></Singlecountry>)
             }
+            </div>
+            
             
         </div>
     );
--- Milestone 7/module 40/rest-countries-project/src/countries/country/singleCountry/singlecountry.jsx
+++ Milestone 7/module 40/rest-countries-project/src/countries/country/singleCountry/singlecountry.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 const Singlecountry = ({singleCountry}) => {
    const [visited,setVisited]=useState(false)
+   const [count,setCount]=useState(0)
     const handleClick=()=>{
         // setVisited(true)
         // if(visited===true){
@@ -11,17 +12,20 @@ const Singlecountry = ({singleCountry}) => {
         //     setVisited(true)
         // }
         setVisited(!visited)
+        setCount(count+1)
     }
     return (
-        <div className={`border border-4  border-red-500 rounded-2xl flex flex-col justify-between gap-3 p-6 ${visited&&"bg-yellow-50"}`}>
-            <h1 className={`text-center text-base font-normal ${visited &&"text-black"}`}>Name:{singleCountry.name.common} </h1>
+        <div className={` border-4  border-red-500 rounded-2xl flex flex-col justify-between gap-3 p-6 ${visited&&"bg-yellow-50"}`}>
+            <h1 className={`text-center text-base font-normal ${visited &&"text-black"}`}>Name: {singleCountry.name.common} </h1>
       
            <img src={singleCountry.flags.png} alt="" />
       
+         
            <button onClick={handleClick}> 
             {visited?"visited":"not visited"}
           
            </button>
+          
       
            
         </div>
