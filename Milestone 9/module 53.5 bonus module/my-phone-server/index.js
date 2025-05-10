537730d7e494f1ab9771cd78cf110fb09c5ce204
programmarimran <programmarimran@gmail.com>
2025-05-10 12:45:14 +0600

    added a expresss install and simple api server created module 53.5

---
+++ Milestone 9/module 53.5 bonus module/my-phone-server/index.js
@@ -0,0 +1,22 @@
+const express=require('express');
+const phones = require('./phones.json');
+const app=express();
+const port=3000;
+
+app.get("/phones",(req ,res)=>{
+     res.send(phones)
+})
+app.get("/phones/:id",(req,res)=>{
+    const id=parseInt(req.params.id);
+    const phone=phones.find(phone=>phone.id===id)
+    res.send(phone)
+    console.log("this is my params id",id)
+})
+app.get('/',(req,res)=>{
+    res.send("this is my phones fff  api oooooooooooo")
+
+})
+
+app.listen(port,()=>{
+    console.log('it is my first phones api ddddddddd')
+})
