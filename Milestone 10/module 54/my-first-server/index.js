7a7515abcc9a760098cad7264cf3be369a16291c
programmarimran <programmarimran@gmail.com>
2025-05-12 19:43:28 +0600

    added a commit of git ignor file checked and server site and client side my first project is created |

--- Milestone 10/module 54/my-first-server/index.js
+++ Milestone 10/module 54/my-first-server/index.js
@@ -1,23 +1,32 @@
 const express=require("express");
+var cors = require('cors')
 const app=express();
-const port=3000;
-
-const profile=[{
-    name:"imran",
-    age:22,
-    image:"http",
-    father:"Md Kalam"
-},
-{
-    name:"Anisul",
-    age:25,
-    image:"http",
-    father:"Md Kalam"
-},]
+const port=process.env.PORT|| 3000;
+app.use(express.json())
+app.use(cors())
+const profile=[
+    {id:1,name:"Imran",email:"imran@gmail.com"},
+    {id:2,name:"Anisul",email:"anisul@gmail.com"},
+    {id:3,name:"Taiyeb",email:"taiteb@gmail.com"}
+]
 
 app.get("/",(req,res)=>{
-    const user=profile
+    
+    res.send("This is a first server site in my life")
+})
+
+app.get("/users",(req,res)=>{
+    const user=profile;
     res.send(user)
+})
+
+app.post("/users",(req,res)=>{
+    const newUser=req.body;
+    console.log(newUser)
+    newUser.id=profile.length+1;
+    profile.push(newUser)
+
+    res.send(newUser)
 })
 
 app.listen(port,()=>{
